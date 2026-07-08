83dee6ea28773093ad72fe371abaa6c150d6557b
DorVeytarn <63097757+DorVeytarn@users.noreply.github.com>
2026-07-08 19:19:39 +0300

    Add UI and keymap module

---
+++ houdini_numpad/ui.py
@@ -0,0 +1,24 @@
+import bpy
+
+
+def register():
+    """Register keyboard mapping for the addon"""
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+    
+    if kc:
+        km = kc.keymaps.new(name="Window", space_type='EMPTY', region_type='WINDOW', modal=False)
+        kmi = km.keymap_items.new('wm.houdini_numpad_editor', 'MIDDLEMOUSE', 'PRESS')
+
+
+def unregister():
+    """Unregister keyboard mapping"""
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+    
+    if kc:
+        for km in kc.keymaps:
+            if km.name == "Window":
+                for kmi in km.keymap_items:
+                    if kmi.idname == 'wm.houdini_numpad_editor':
+                        km.keymap_items.remove(kmi)
